cd6b3093769cd32ac6b524e1145283890ecc6ae0
Laura Garcia <lauraga41@gmail.com>
2016-07-12 22:40:03 -0500

    Extensions tolk

--- app/helpers/conductor/application_helper.rb
+++ app/helpers/conductor/application_helper.rb
@@ -1,4 +1,9 @@
 module Conductor
   module ApplicationHelper
+    def tolk_link
+      link_to_if(defined?(Tolk), "Tolk", '/tolk') do
+      link_to("Tolk", "http://www.github.com/tolk/tolk", target: "_blank", data: { confirm: "This extension is not installed." })
+     end
+    end
   end
 end
--- lib/conductor/engine.rb
+++ lib/conductor/engine.rb
@@ -1,5 +1,6 @@
 module Conductor
   class Engine < ::Rails::Engine
+    require 'jquery-rails'
     isolate_namespace Conductor
 
     initializer "conductor.assets.precompile" do |app|
