93460d17ca25e33fb7443031b1702fb13fd9efda
Laura Garcia <lauraga41@gmail.com>
2016-07-30 15:40:14 -0500

    Documentation

--- app/helpers/conductor/application_helper.rb
+++ app/helpers/conductor/application_helper.rb
@@ -2,8 +2,32 @@ module Conductor
   module ApplicationHelper
     def tolk_link
       link_to_if(defined?(Tolk), "Tolk", '/tolk') do
-      link_to("Tolk", "http://www.github.com/tolk/tolk", target: "_blank", data: { confirm: "This extension is not installed." })
-     end
+        link_to("Tolk", "http://www.github.com/tolk/tolk", target: "_blank", data: { confirm: "This extension is not installed." })
+      end
+    end
+
+    def documentation_rails
+      if File.exist?(File.join(Rails.root, 'doc', 'api', 'index.html'))
+        link_to_doc('Rails API', 'api')
+      else
+        link_to("Rails API", "http://api.rubyonrails.org/", target: "_blank")
+      end
+    end
+
+    def documentation_guides
+      if File.exist?(File.join(Rails.root, 'doc', 'guides', 'index.html'))
+        link_to_doc('Rails Guides', 'guides')
+      else
+        link_to("Rails Guides", "http://guides.rubyonrails.org/", target: "_blank")
+      end
+    end
+
+    def documentation_app
+      if File.exist?(File.join(Rails.root, 'doc', 'app', 'index.html'))
+        link_to_doc('Your App', 'app')
+      else
+        link_to("Your App","#", data: { confirm: "rake doc:app generates documentation for your application in doc/app." })
+      end
     end
   end
 end
