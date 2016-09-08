6ffead68bb29fa8206e7be13e35aaf6631fdc80f
Laura Garcia <lauraga41@gmail.com>
2016-09-08 09:45:53 -0500

    Manage Migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Conductor::Engine.routes.draw do
   resource :database
+
+  resources :migrations, only: :index do
+    member do
+      put :up
+      put :down
+    end
+  end
   get '/routes' => 'routes#index', as:'routes'
   get '/annotations' => 'annotations#index', as:'annotations'
   get '/statistics' => 'statistics#index', as:'statistics'
--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  resources :posts
   mount Conductor::Engine => "/conductor"
 end
